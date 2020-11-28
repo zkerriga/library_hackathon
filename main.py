417f38004e11975c57edf2c2a10190b68dc444e5
zkerriga <daniil.63@yandex.ru>
2020-11-28 19:48:55 +0300

    Add csv worked

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import csv
 from Date import *
 
 def getSourceList():
+	sourceList = []
 	with open("temporal-thesaurus-analytical-corpora-test.csv", 'r') as file:
-		sourceList = list(csv.reader(file))
-	return sourceList
+		reader = csv.reader(file)
+		for line in reader:
+			sourceList.append(line[0])
+	return sourceList[1:]
 
 def writeOutputResult(sourceList, datesList):
 	result = "result.csv"
 	with open(result, 'w') as file:
-		writer = csv.writer(file, delimiter=',')
+		writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+		writer.writerow(["Id", "Expected"])
 		for i in range(len(sourceList)):
-			writer.writerow(",".join([sourceList[i], datesList[i]]))
+			writer.writerow([sourceList[i], datesList[i]])
 
 def createDateObjectsList(length):
 	return [Date() for _ in range(length)]
