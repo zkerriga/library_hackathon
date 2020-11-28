5923726a8ff6680c7bcc688bd5ff84adbfb0f5b0
Aleksandr Marakulin <sasha.marakulin@mail.ru>
2020-11-28 23:18:36 +0300

    Add converting russian number to str

--- filters/text_filter.py
+++ filters/text_filter.py
@@ -1,5 +1,6 @@
 import re
 from typing import Final
+from .ru_number_to_str import replaceRuNumberToStr
 
 REPLACE_PAIRS: Final = [
 	("ё", "е"),
@@ -41,4 +42,5 @@ def textFilter(sourceStr: str):
 	found = re.search(DATE_FORMS_REGEX, newStr)
 	if found:
 		newStr = newStr.replace(found[0], found[1])
+	newStr = replaceRuNumberToStr(newStr)
 	return newStr
