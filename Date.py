0927b00955afe2b83b6c82d3a123539625cbe37a
zkerriga <daniil.63@yandex.ru>
2020-11-28 19:14:15 +0300

    Add Date class, basic

---
+++ Date.py
@@ -0,0 +1,44 @@
+import datetime
+
+
+class Period(object):
+	"""docstring for Period"""
+
+	dateSeparator = '/'
+
+	def __init__(self, dataStart, dataEnd):
+		super(Period, self).__init__()
+
+		self.dataStart = dataStart
+		self.dataEnd = dataEnd
+
+
+class PartOfDay(enumerate):
+	AM = 0
+	PM = 1
+
+
+class Date(object):
+	"""docstring for Data"""
+
+	def __init__(self):
+		super(Date, self).__init__()
+		self.now = datetime.datetime.now()
+
+		self.year = int(self.now.year)
+		self.month = int(self.now.month)
+		self.day = int(self.now.day)
+
+		self.weekDay = self.now.weekday()  # [0,6]
+
+		if self.now.hour > 12:
+			self.partOfDay = PartOfDay.PM
+		else:
+			self.partOfDay = PartOfDay.AM
+
+		self.hours = int(self.now.hour)
+		self.minutes = int(self.now.minute)
+		self.seconds = int(self.now.second)
+
+	def __str__(self):
+		return f"{self.year}-{self.month}-{self.day}T{self.hours // 10}{self.hours % 10}:{self.minutes // 10}{self.minutes % 10}"
--- main.py
+++ main.py
@@ -1,54 +1,19 @@
-# import Date
 import csv
-from natasha import (
-	Segmenter,
-	MorphVocab,
-	
-	NewsEmbedding,
-	NewsMorphTagger,
-	NewsSyntaxParser,
-	NewsNERTagger,
-	
-	PER,
-	NamesExtractor,
-	DatesExtractor,
-	MoneyExtractor,
-	AddrExtractor,
-
-	Doc
-)
+from Date import *
 
 def main():
 	print("Start:")
-	text:str = ""
+	text = ""
 
 	with open("temporal-thesaurus-analytical-corpora-test.csv", 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
 			text += row[0] + ' ; '
 
-	segmenter = Segmenter()
-	morph_vocab = MorphVocab()
+def test():
+	date = Date()
+	print(date)
 
-	emb = NewsEmbedding()
-	morph_tagger = NewsMorphTagger(emb)
-	syntax_parser = NewsSyntaxParser(emb)
-	ner_tagger = NewsNERTagger(emb)
-
-	names_extractor = NamesExtractor(morph_vocab)
-	dates_extractor = DatesExtractor(morph_vocab)
-	money_extractor = MoneyExtractor(morph_vocab)
-	addr_extractor = AddrExtractor(morph_vocab)
-	
-	# doc = Doc(text)
-	# doc.segment(segmenter)
-	# print(doc)
-	# print(doc.sents[:2])
-	# print(doc.tokens[:5])
-
-	listDates = list(dates_extractor(text))
-	with open("result.csv", 'w') as fileOut:
-		for el in listDates:
-			fileOut.write(str(el) + "\n")
-
-main()
+if __name__ == "__main__":
+	# main()
+	test()
